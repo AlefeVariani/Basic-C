12401e0b4faece3ad0b80e8288e3cf5f1115a21d
Alefe Variani <alefevariani@unochapeco.edu.br>
2014-04-18 16:10:47 -0300

    nova pasta funcoes e novos arquivos na pasta abc

---
+++ abc/troca.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+void troca(int a, int b);
+
+void troca(int a, int b){
+	int temp;
+
+	temp= a;
+	a= b;
+	b= temp;
+
+}
+
+int main(void)
+{
+	int val1, val2;
+	val1= 10;
+	val2= 15;
+
+	printf("val1 = %d    val2 = %d \n", val1, val2);
+
+	troca(val1, val2);
+
+	printf("val1= %d     val2= %d \n", val1, val2);
+
+	return 0;
+}
