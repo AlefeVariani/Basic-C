e486846b7f0641df6c673e1255026019101afdf7
Alefe Variani <alefevariani@unochapeco.edu.br>
2014-05-08 20:46:33 -0300

    itentação e valor fixo

--- buscas/Mergesort.c
+++ buscas/Mergesort.c
@@ -6,91 +6,91 @@ void mergesort(char *matriz,int inicio,int meio,int final);
 void merge(char *matriz,int inicio,int final);
 
 int main () {
-    char s[80];
-    printf ("Informe uma string a ser ordenada: ");
-    gets(s);
-    merge (s, 0, strlen(s)-1);
-    printf("\nA string ordenada eh: %s. \n", s);
-    getchar();
+	char s[80];
+	printf ("Informe uma string a ser ordenada: ");
+	strcpy(s,"bcdaoertlfsqw");;
+	merge (s, 0, strlen(s)-1);
+	printf("\nA string ordenada eh: %s. \n", s);
+	getchar();
 }
 void mergesort(char *matriz,int inicio,int meio,int final)
 {
-    int i,j,k,l,b[20];
-    l=inicio;
-    i=inicio;
-    j=meio+1;
-    int ii,iii;
+	int i,j,k,l,b[20];
+	l=inicio;
+	i=inicio;
+	j=meio+1;
+	int ii,iii;
 
-    ii= strlen(matriz);
-    while((l<=meio)&&(j<=final))
-    {
-        if(matriz[l]<=matriz[j])
-        {
-            b[i]=matriz[l];
-            l++;
-        }
-        else
-        {
-            b[i]=matriz[j];
-            j++;
-        }
-        i++;
-        iii = 0;
-        while(iii<ii)
-        {
-            printf("%c\t", matriz[iii]);
-            iii++;
-        }
-        printf("\n");
-    }
-    if(l>meio)
-    {
-        for(k=j;k<=final;k++)
-        {
-            b[i]=matriz[k];
-            i++;
-        }
-        iii = 0;
-        while(iii<ii)
-        {
-            printf("%c\t", matriz[iii]);
-            iii++;
-        }
-        printf("\n");
-    }
-    else
-    {
-        for(k=l;k<=meio;k++)
-        {
-            b[i]=matriz[k];
-            i++;
-        }
-        iii = 0;
-        while(iii<ii)
-        {
-            printf("%c\t", matriz[iii]);
-            iii++;
-        }
-        printf("\n");
-    }
-    for(k=inicio;k<=final;k++)
-    {
-        matriz[k]=b[k];
-    }
+	ii= strlen(matriz);
+	while((l<=meio)&&(j<=final))
+	{
+		if(matriz[l]<=matriz[j])
+		{
+			b[i]=matriz[l];
+			l++;
+		}
+		else
+		{
+			b[i]=matriz[j];
+			j++;
+		}
+		i++;
+		iii = 0;
+		while(iii<ii)
+		{
+			printf("%c\t", matriz[iii]);
+			iii++;
+		}
+		printf("\n");
+	}
+	if(l>meio)
+	{
+		for(k=j;k<=final;k++)
+		{
+			b[i]=matriz[k];
+			i++;
+		}
+		iii = 0;
+		while(iii<ii)
+		{
+			printf("%c\t", matriz[iii]);
+			iii++;
+		}
+		printf("\n");
+	}
+	else
+	{
+		for(k=l;k<=meio;k++)
+		{
+			b[i]=matriz[k];
+			i++;
+		}
+		iii = 0;
+		while(iii<ii)
+		{
+			printf("%c\t", matriz[iii]);
+			iii++;
+		}
+		printf("\n");
+	}
+	for(k=inicio;k<=final;k++)
+	{
+		matriz[k]=b[k];
+	}
 
 }
 
 void merge(char *matriz,int inicio,int final)
 {
-    int meio;
-    int ii,iii;
+	int meio;
+	int ii,iii;
 
-    ii= strlen(matriz);
-    if(inicio<final)
-    {
-        meio=(inicio+final)/2;
-        merge(matriz,inicio,meio);
-        merge(matriz,meio+1,final);
-        mergesort(matriz,inicio,meio,final);
-    }
+	ii= strlen(matriz);
+	if(inicio<final)
+	{
+		meio=(inicio+final)/2;
+		merge(matriz,inicio,meio);
+		merge(matriz,meio+1,final);
+		mergesort(matriz,inicio,meio,final);
+	}
 }
--- buscas/Quicksort.c
+++ buscas/Quicksort.c
@@ -1,59 +1,55 @@
-#include <string.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+// Quicksort
+void qs(char *matriz, int esq, int dir) {
+	register int i, j;
+	char x, y;
+	int ii,iii;
 
+	ii = strlen(matriz);
+	i=esq;
+	j=dir;
+	x=matriz[(esq+dir)/2];
 
- 
-// Quicksort 
-void qs(char *matriz, int esq, int dir) { 
-    register int i, j; 
-    char x, y; 
-    int ii,iii;
-    
-    ii = strlen(matriz);
-    i=esq; 
-    j=dir; 
-    x=matriz[(esq+dir)/2]; 
+	do {
+		while (matriz[i]<x && i<dir) i++;
+		while (x<matriz[j] && j>esq) j--;
+		if (i<=j) {
 
-    do { 
-        while (matriz[i]<x && i<dir) i++; 
-            while (x<matriz[j] && j>esq) j--; 
-            if (i<=j) { 
-                
-                iii = 0;
-                while(iii<ii)
-                {
-                    printf("%c\t", matriz[iii]);
-                    iii++;
-                }
-                printf("\n");
-                y=matriz[i]; 
-                matriz[i]=matriz[j]; 
-                matriz[j]=y;
-                i++; 
-                j--; 
- 
-                
-            } 
-    } while (i<=j); 
+			iii = 0;
+			while(iii<ii)
+			{
+				printf("%c", matriz[iii]);
+				iii++;
+			}
+			printf("\n");
+			y=matriz[i];
+			matriz[i]=matriz[j];
+			matriz[j]=y;
+			i++;
+			j--;
 
-    if (esq<j) 
-        qs(matriz, esq, j); 
-    if (i<dir) 
-        qs(matriz, i, dir); 
+		}
+	} while (i<=j);
+
+	if (esq<j)
+		qs(matriz, esq, j);
+	if (i<dir)
+		qs(matriz, i, dir);
 }
 
-// funcao de inicializacao do quicksort 
-void quick (char *matriz, int numEle) { 
-        qs (matriz, 0, numEle-1); 
-} 
- 
-int main () { 
-    char s[80]; 
-    printf ("Informe uma string a ser ordenada: "); 
-    gets(s); 
-    quick (s, strlen(s)); 
-    printf("\nA string ordenada eh: %s. \n", s); 
-    getchar(); 
+// funcao de inicializacao do quicksort
+void quick (char *matriz, int numEle) {
+	qs (matriz, 0, numEle-1);
+}
+
+int main () {
+	char s[80];
+	printf ("Informe uma string a ser ordenada: ");
+    strcpy(s,"bcdaoertlfsqw");//informa caracteres
+    quick (s, strlen(s));
+    printf("\nA string ordenada eh: %s. \n", s);
+    getchar();
 }
